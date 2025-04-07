65d8909dba7a809ddf391e81aa7f1f415eb1a54a
Blixt <me@blixt.nyc>
2025-04-07 20:37:39 +0200

    Just clean up the code a bit

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,17 @@
-import React from "react";
-import { createRoot } from 'react-dom/client';
+import { createRoot } from "react-dom/client";
 import { AnimatePresence, motion } from "motion/react";
 
 function Test() {
-    return (
-        <AnimatePresence mode="wait" initial={false}>
-            <motion.div>Hello World</motion.div>
-        </AnimatePresence>
-    )
+  return (
+    <AnimatePresence mode="wait" initial={false}>
+      <motion.div>Hello World</motion.div>
+    </AnimatePresence>
+  );
 }
 
-const domNode = document.getElementById('root');
+const domNode = document.getElementById("root");
+if (!domNode) {
+  throw new Error("Root element not found");
+}
 const root = createRoot(domNode);
 root.render(<Test />);
